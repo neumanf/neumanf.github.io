2d615963a788d23a16b6c69ca47f184084dc246e
neumanf <fabricionewman@protonmail.com>
2022-08-10 10:08:30 -0300

    remove unaccessible links from navbar

--- src/App.js
+++ src/App.js
@@ -9,16 +9,8 @@ const navBarLinks = [
     label: "Home",
   },
   {
-    link: "/about",
-    label: "About",
-  },
-  {
     link: "/projects",
     label: "Projects",
-  },
-  {
-    link: "/contact",
-    label: "Contact",
   },
 ];
 
