d3f794090126f34e27fa094cb41aa1fc0522cf3a
neumanf <fabricionewman@protonmail.com>
2024-05-27 19:11:08 -0300

    feat: v6

---
+++ src/constants/socials.ts
@@ -0,0 +1,11 @@
+type SocialsItem = {
+    icon: string;
+    url: string;
+};
+
+export const socialsList: SocialsItem[] = [
+    {
+        icon: "brand-github",
+        url: "https://github.com/neumanf",
+    },
+];
